76cbb2fad026ec711e88c55e9e71cf6a59068d1b
Diego Sagastume <dsagastume.castillo@gmail.com>
2014-04-19 11:06:56 -0600

    Fixes.

--- js/index.js
+++ js/index.js
@@ -256,7 +256,7 @@ var app = {
 	},
 	playTune : function(status) {
 
-		var aliado1 = new Media("/android_asset/aliado1.mp3",
+		var aliado1 = new Media("/android_asset/www/aliado1.mp3",
 			function() {
 				console.log("Play OK");
 			},
@@ -270,7 +270,7 @@ var app = {
 			}
 		);
 
-		var enemigo1 = new Media("/android_asset/enemigo1.mp3",
+		var enemigo1 = new Media("/android_asset/www/enemigo1.mp3",
 			function() {
 				console.log("Play OK");
 			},
@@ -284,7 +284,7 @@ var app = {
 			}
 		);
 
-		var neutral1 = new Media("/android_asset/neutral1.mp3",
+		var neutral1 = new Media("/android_asset/www/neutral1.mp3",
 			function() {
 				console.log("Play OK");
 			},
